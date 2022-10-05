11cfd07c603d883002bbeca46690e60c89c9fb94
IAmBill030202 <nhantvde160428@fpt.edu.vn>
2022-10-05 13:26:28 +0700

    First Component

---
+++ src/components/App.js
@@ -0,0 +1,20 @@
+import { Navbar, NavbarBrand } from "reactstrap";
+import React, { Component } from "react";
+// import "./css/App.css";
+import Menu from "./MenuComponents";
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
+          </div>
+          <Menu />
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default App;
